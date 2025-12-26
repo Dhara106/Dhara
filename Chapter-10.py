6608e2df6a3dd680f6cfac986726516ba244081d
Dhara106 <sojitradhara64@gmail.com>
2025-12-26 13:31:57 +0530

    Add files via upload

---
+++ Chapter-10.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 23/12/2025
+# # Chapter-10
+# ## Visualization & Streamlit UI
+# ### Matplotlib Library
+# - plot, scatterplot, Bas chart, Area plot, Histogram, Pie chart with different parameters.
+# ### Basics of streamlit UI
+# - creating a UI
+# - I/P widgets
+# - display O/Ps
+# - streamlit with charts.
+
+# In[ ]:
+
+
+
+
